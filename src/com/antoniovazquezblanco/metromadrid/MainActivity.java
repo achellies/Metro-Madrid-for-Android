6ef0a208129d998a2bb01a3117f16d2a473669ea
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-12-22 18:15:31 +0100

    Add GPL v3 headers and refactor some code.

--- src/com/antoniovazquezblanco/metromadrid/MainActivity.java
+++ src/com/antoniovazquezblanco/metromadrid/MainActivity.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Metro Madrid for Android.
+ *
+ * Metro Madrid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Metro Madrid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Metro Madrid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.antoniovazquezblanco.metromadrid;
 
 import android.app.TabActivity;
@@ -15,8 +33,6 @@ public class MainActivity extends TabActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO: Fragments+Pager: http://developer.android.com/resources/samples/Support4Demos/src/com/example/android/supportv4/app/FragmentTabsPager.html
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_main);
 
